432fe736a7ccf7af8ec9806f06409ec2ff389da8
Linas Nikiperavicius <linas@linasdev.com>
2020-06-14 16:13:52 +0300

    Revamp constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,13 @@
-export const PLAYER_SIZE = 64; // Height of the player in-game.
-export const PLAYER_SHEET_COLS = 64; // Number of columns in player's spritesheet.
-export const PLAYER_SHEET_ROWS = 8; // Number of rows in player's spritesheet.
-export const PLAYER_ANIM_FPS = 10; // Framerate of player animation.
-export const DOUBLE_TAP_TIMING = 200; // The maximum amount of time between touches for them to count as a double-tap. Specified in milliseconds.
+// Control related:
+// Max delay between touches that still count as a double-tap (specified in milliseconds):
+export const DOUBLE_TAP_TIMING = 200;
+
+// Player related:
+// Height of the player in-game:
+export const PLAYER_SIZE = 64;
+// Number of columns in the player spritesheet:
+export const PLAYER_SHEET_COLS = 64;
+// Number of rows in the player spritesheet:
+export const PLAYER_SHEET_ROWS = 8;
+// Framerate of player animation:
+export const PLAYER_ANIM_FPS = 10;
